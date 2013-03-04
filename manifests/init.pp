ca6ac53db7f1af284107e30d21224fbd8edce6f7
Vincent Ambo <vincent@spotify.com>
2013-03-04 18:15:51 +0100

    Readded assistive devices enabling

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,7 +14,24 @@ class xnomad {
     source   => 'puppet:///modules/xnomad/.xnomad'
   }
 
+  # Enable access for assistive devices
+  file { '/var/db/.AccessibilityAPIEnabled':
+    content => 'a',
+    owner   => 'root',
+    group   => 'wheel',
+    mode    => '0444',
+  }
 
+  # Replace rounded corners on 10.8.2 (untested on others)
+  if $::macosx_productversion == '10.8.2' {
+    file { '/System/Library/PrivateFrameworks/CoreUI.framework/Versions/A/Resources/ArtFile.bin':
+      source => 'puppet:///modules/xnomad/ArtFile.bin'
+    }
+
+    file { '/System/Library/PrivateFrameworks/CoreUI.framework/Versions/A/Resources/SArtFile.bin':
+      source => 'puppet:///modules/xnomad/SArtFile.bin'
+    }
+  }
 
   file { '/var/db/.AccessibilityAPIEnabled':
     content => 'a',
