4f6282d1b43ca8aa2fc9c36879ff30dc4abe6200
Vincent Ambo <vincent@spotify.com>
2013-03-04 18:33:23 +0100

    foo

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,9 +14,30 @@ class xnomad {
     source   => 'puppet:///modules/xnomad/.xnomad'
   }
 
+
+  # Enable access for assistive devices
+  file { '/var/db/.AccessibilityAPIEnabled':
+    content => 'a',
+    owner   => 'root',
+    group   => 'wheel',
+    mode    => '0444',
+  }
+
+  # Replace rounded corners on 10.8.2 (untested on others)
+  if $::macosx_productversion == '10.8.2' {
+    file { '/System/Library/PrivateFrameworks/CoreUI.framework/Versions/A/Resources/ArtFile.bin':
+      source => 'puppet:///modules/xnomad/ArtFile.bin'
+    }
+
+    file { '/System/Library/PrivateFrameworks/CoreUI.framework/Versions/A/Resources/SArtFile.bin':
+      source => 'puppet:///modules/xnomad/SArtFile.bin'
+    }
+  }
+
+
   osx_login_item { 'Xnomad':
     path     => '/Applications/Xnomad.app',
-    require  => [File["/Users/${::boxen_user}/.xnomad"]]
+    require  => File["/Users/${::boxen_user}/.xnomad"]
   }
   osx_login_item { 'Shadowkiller':
     path     => '/Applications/Shadowkiller.app'
