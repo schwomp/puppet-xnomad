c04c1b3aad36770c5c654a22e1db76302eb732ce
Vincent Ambo <vincent@spotify.com>
2013-03-04 18:27:09 +0100

    I don't even know what I'm doing anymore :)

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,35 +14,9 @@ class xnomad {
     source   => 'puppet:///modules/xnomad/.xnomad'
   }
 
-  # Enable access for assistive devices
-  file { '/var/db/.AccessibilityAPIEnabled':
-    content => 'a',
-    owner   => 'root',
-    group   => 'wheel',
-    mode    => '0444',
-  }
-
-  # Replace rounded corners on 10.8.2 (untested on others)
-  if $::macosx_productversion == '10.8.2' {
-    file { '/System/Library/PrivateFrameworks/CoreUI.framework/Versions/A/Resources/ArtFile.bin':
-      source => 'puppet:///modules/xnomad/ArtFile.bin'
-    }
-
-    file { '/System/Library/PrivateFrameworks/CoreUI.framework/Versions/A/Resources/SArtFile.bin':
-      source => 'puppet:///modules/xnomad/SArtFile.bin'
-    }
-  }
-
-  file { '/var/db/.AccessibilityAPIEnabled':
-    content => 'a',
-    owner   => 'root',
-    group   => 'wheel',
-    mode    => '0444',
-  }
-
   osx_login_item { 'Xnomad':
     path     => '/Applications/Xnomad.app',
-    require  => [File["/Users/${::boxen_user}/.xnomad"], File['/var/db/.AccessibilityAPIEnabled']]
+    require  => [File["/Users/${::boxen_user}/.xnomad"]]
   }
   osx_login_item { 'Shadowkiller':
     path     => '/Applications/Shadowkiller.app'
